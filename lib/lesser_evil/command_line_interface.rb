7a01221bdbefd3891af27384869889ef22c921c2
Adam Cooper <amcooper@gmail.com>
2016-11-02 17:38:28 -0400

    Intro display - candidate portraits

--- lib/lesser_evil/command_line_interface.rb
+++ lib/lesser_evil/command_line_interface.rb
@@ -2,6 +2,39 @@ class LesserEvil::CommandLineInterface
 
 	def initialize
 	end
+
+	def intro_display
+		20.times { puts }
+
+		# hillary_image = File.open("assets/hillary.txt", "r")
+		# donald_image = File.open("assets/donald.txt", "r")
+		# hillary_contents = hillary_image.read
+		# donald_contents = donald_image.read
+		File.open("assets/hillary.txt", "r") do |file|
+			file.readlines.each do |line|
+				print line.white.on_black
+				sleep 0.05
+			end
+		end
+		# puts hillary_contents
+		puts 
+
+		File.open("assets/donald.txt", "r") do |file|
+			file.readlines.each do |line|
+				print line.white.on_black
+				sleep 0.05
+			end
+		end
+
+		puts
+		puts "-----------------------------------".red
+		puts "LESSER EVIL  ••••••••••••••••••••••".red
+		puts
+		puts "Peruse the angriest election tweets".red
+		puts "-----------------------------------\n\n\n".red
+		# hillary_image.close
+		# donald_image.close
+	end
   
   def input_validation(valid_array)
   	input = gets.chomp.downcase
@@ -13,7 +46,7 @@ class LesserEvil::CommandLineInterface
   end
 
 	def call
-    puts "LESSER EVIL\n"
+    intro_display
     print "Trump or Clinton? Choose a candidate: "
     candidate = input_validation(['trump','clinton'])
     # candidate = 'trump' # debug
